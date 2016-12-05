e32bf83d0c2f4a2ac434ab2909b9c27ff0d54581
sxc588 <54472951@qq.com>
2016-12-06 00:37:52 +0800

    index cbb service

--- MicroService/CBBService/src/main/java/com/github/support/cbbservice/json/DateUtil.java
+++
@@ -1,6 +0,0 @@
-package com.github.support.cbbservice.json;
-
-public interface DateUtil
-{
-	final String yyyy_MM_dd_HH_mm_ss = "yyyy_MM_dd_HH_mm_ss";
-}
---
+++ MicroService/CBBService/web-common/src/main/java/com/github/support/cbb/ehcache/quartz/job/QuartzJob.java
@@ -0,0 +1,36 @@
+package com.github.support.cbb.ehcache.quartz.job;
+
+import javax.jms.Destination;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.github.support.cbb.ehcache.jms.JMSMessageService;
+
+public class QuartzJob implements Job
+{
+	private Logger logger = LoggerFactory.getLogger(QuartzJob.class);
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException
+	{
+		logger.info("★★★★★★★★★★★ 动态Job执行开始");
+		
+		JMSMessageService jmsMessageService = HandlerServiceManager.getInstance().getJmsMessageService();
+		Destination destination=  HandlerServiceManager.getInstance().getDestination();
+		
+
+		for (int i = 1000; i < 2000; i++)
+		{
+			jmsMessageService.sendMessage(destination, "你好，生产者！这是队列消息：" + (i + 1));
+		}
+		
+	//	List<Sysconfig> abc = sysconfigMapper.getAllAccount();
+//		logger.info(abc.toString());
+//	producerService.sendMessage("DDF");
+		
+		logger.info("★★★★★★★★★★★  动态Job执行结束");
+	}
+
+}
